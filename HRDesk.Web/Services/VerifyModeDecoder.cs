32dc89878d5facc45980203d26b44711f6961a75
sachin mandal <sachin.balarbulders@gmail.com>
2026-07-10 19:18:58 +0530

    UI Updates, VerifyMode Face Fix, and Birthday Poster adjustments

--- HRDesk.Web/Services/VerifyModeDecoder.cs
+++ HRDesk.Web/Services/VerifyModeDecoder.cs
@@ -13,7 +13,10 @@ public static class VerifyModeDecoder
         return baseMode switch
         {
             1 or 4 or 5 or 7 or 51 or 101 or 151 => "Fingerprint",
-            30 or 31 or 32 or 33 or 34 => "Face",
+            30 or 31 or 32 or 33 or 34 or 
+            80 or 81 or 82 or 83 or 84 or 
+            130 or 131 or 132 or 133 or 134 or 
+            180 or 181 or 182 or 183 or 184 => "Face",
             2 or 6 or 52 or 102 or 152 => "Password",
             3 or 53 or 103 or 153 => "Card",
             10 or 11 or 12 or 13 or 14 or 20 or 21 or 22 or 23 => "Access Control Event",
